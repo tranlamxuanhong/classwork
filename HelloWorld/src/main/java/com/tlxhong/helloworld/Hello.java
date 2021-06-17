67638eb7ccd11a6460009eb61731fa90238b2ef1
Tran Lam Xuan Hong <tlxhong1108@gmail.com>
2021-06-17 16:39:22 -0700

    My First Hello World Project

---
+++ HelloWorld/src/main/java/com/tlxhong/helloworld/Hello.java
@@ -0,0 +1,17 @@
+/*
+ * author: Tran Lam Xuan Hong
+ * email: tlxhong1108@gmail.com
+ * date: Jun 17 2021
+ * purpose: My first Hello World program in an IDE
+ */
+package com.tlxhong.helloworld;
+
+/**
+ *
+ * @author tranlamxuanhong
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Hong");
+    }
+}
